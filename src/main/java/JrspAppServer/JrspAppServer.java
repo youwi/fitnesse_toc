036968c15c71b6ff5d874b81a56d5dbe7191b135
youwi <youwi@github.com>
2015-07-08 18:15:10 +0800

    add JRSP

---
+++ src/main/java/JrspAppServer/JrspAppServer.java
@@ -0,0 +1,54 @@
+package main.java.JrspAppServer;
+
+import java.io.IOException;
+
+import main.java.TestHttpClient.HttpClient;
+import main.java.TestHttpClient.HttpRequestCallback;
+import main.java.constants.ConfigConstants;
+import main.java.utils.Data;
+import main.java.utils.JSONParse;
+
+import org.json.JSONObject;
+
+public class JrspAppServer {
+	Data data;
+	JSONParse jp;
+	String URL;
+
+	public JrspAppServer(String URL) {
+		this.data = new Data();
+		this.jp = new JSONParse();
+		this.URL = URL;
+	}
+
+	public void setParam(String name, String value, String type)
+			throws Exception {
+		data.setParameters(name, value, type);
+	}
+
+	public String getParam(String key) {
+		return jp.getResult(key);
+	}
+
+	public boolean testRun(String param) throws IOException {
+		if (null == param.trim()) {
+			System.out.println("null paramters!!");
+			return false;
+		} else {
+			HttpClient testRequst = new HttpClient();
+			String responseBody = testRequst.httpPostRequest(
+					ConfigConstants.JRSP_BASE_URL + URL,
+					new HttpRequestCallback() {
+						@Override
+						public String addParam() {
+							// TODO Auto-generated method stub
+							JSONObject obj = new JSONObject();
+							return data.getAddParam(obj);
+						}
+					});
+			JSONObject objResponse = new JSONObject(responseBody);
+			jp.parseJson(objResponse);
+			return jp.checkParam(param);
+		}
+	}
+}
--- src/main/java/constants/ConfigConstants.java
+++ src/main/java/constants/ConfigConstants.java
@@ -4,6 +4,11 @@ public class ConfigConstants {
     public final static String WKZF_BASE_URL = "http://10.0.18.79:8117";// test
   //  public final static String WKZF_BASE_URL = "http://beta.wkzfang.com:8117";// beta
   //  public final static String WKZF_BASE_URL = "http://userapp.wkzfang.com";// prod
+    
+    
+      public final static String JRSP_BASE_URL = "http://10.0.18.79:8104";// test
+  //  public final static String JRSP_BASE_URL = "http://beta.jinrisunpan.com:8104";// beta
+  //  public final static String JRSP_BASE_URL = "appserver.jinrisunpan.com";// prod
 
     public final static String JDBC_DRIVER_CLASS = "com.mysql.jdbc.Driver";
     public final static String JDBC_CONNECT_URL = "jdbc:mysql://192.168.1.14:3306/hims_test?useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false";
