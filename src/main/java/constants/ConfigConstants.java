578824fa0d99923b9ad2b9f510717521704cf24f
youwi <youwi@github.com>
2015-07-10 11:43:27 +0800

    add yfyk

--- src/main/java/constants/ConfigConstants.java
+++ src/main/java/constants/ConfigConstants.java
@@ -9,6 +9,11 @@ public class ConfigConstants {
       public final static String JRSP_BASE_URL = "http://10.0.18.79:8104";// test
   //  public final static String JRSP_BASE_URL = "http://beta.jinrisunpan.com:8104";// beta
   //  public final static String JRSP_BASE_URL = "appserver.jinrisunpan.com";// prod
+      
+      
+      public final static String YFYK_BASE_URL = "http://10.0.18.79:8103";// test
+      //  public final static String YFYK_BASE_URL = "beta.yfyk365.com:8103";// beta
+      //  public final static String YFYK_BASE_URL = "appserver.yfyk365.com";// prod
 
     public final static String JDBC_DRIVER_CLASS = "com.mysql.jdbc.Driver";
     public final static String JDBC_CONNECT_URL = "jdbc:mysql://192.168.1.14:3306/hims_test?useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false";
