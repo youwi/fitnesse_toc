b0def49202f3a67114c2a9aae6d498d7896cfa18
youwi <youwi@github.com>
2015-07-20 15:48:30 +0800

    优化json解析，支持多层级结构

--- src/main/java/utils/JSONParse.java
+++ src/main/java/utils/JSONParse.java
@@ -9,147 +9,164 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JSONParse {
-    private HashMap<String, String> jsonMap = new HashMap<String, String>();
-    private HashMap<String, String> h_jsonMap = new HashMap<String, String>();
-    private ArrayList<String> jsonKey = new ArrayList<String>();
-    private JSONObject obj = null;
-//    private net.sf.json.JSONObject obj2 = null;
-    private boolean flg;
-    private int layer;
+	private HashMap<String, String> jsonMap = new HashMap<String, String>();
+	private ArrayList<String> appendKeys = new ArrayList<String>();
+	private JSONObject obj = null;
 
-    public boolean checkParam(String args) {
-        JSONObject objResponse = this.getObj();
-        boolean flag = true;
-        ArrayList<String> params = new ArrayList<String>();
-        String regex = ",";
-        if (0 == args.split(regex).length) {
-            params.add(args);
-        } else {
-            for (int index = 0; index < args.split(regex).length; index++) {
-                params.add(args.split(regex)[index].trim());
-            }
-            for (String temp : params) {
-                if (!objResponse.has(temp)) {
-                    flag = false;
-                }
-            }
-            return flag;
-        }
-        return false;
-    }
+	public boolean checkParam(String args) {
+		JSONObject objResponse = this.getObj();
+		boolean flag = true;
+		ArrayList<String> params = new ArrayList<String>();
+		String regex = ",";
+		if (0 == args.split(regex).length) {
+			params.add(args);
+		} else {
+			for (int index = 0; index < args.split(regex).length; index++) {
+				params.add(args.split(regex)[index].trim());
+			}
+			for (String temp : params) {
+				if (!objResponse.has(temp)) {
+					flag = false;
+				}
+			}
+			return flag;
+		}
+		return false;
+	}
 
-    public JSONObject getObj() {
-        return this.obj;
-    }
+	public JSONObject getObj() {
+		return this.obj;
+	}
 
-    public void setObj(JSONObject obj) {
-        this.obj = obj;
-    }
-    
-//    public void setObj2(net.sf.json.JSONObject obj) {
-//        this.obj2 = obj;
-//    }
+	public void setObj(JSONObject obj) {
+		this.obj = obj;
+	}
 
-    public String getResult(String Key) {
-        try {
-            System.out.println();
-            System.out.println(h_jsonMap);
-            System.out.println();
-            System.out.println(jsonMap.get(Key).toString());
-            return this.jsonMap.get(Key).toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+	public String getResult(String Key) {
+		try {
+			System.out.println(jsonMap.get(Key).toString());
+			return this.jsonMap.get(Key).toString();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    public void parseJson(JSONObject jsonObject) {
-        this.setObj(jsonObject);
-        parseJson(jsonObject, null);
-    }
-    
-//    public void parseJson2(net.sf.json.JSONObject jsonObject) {
-//        this.setObj2(jsonObject);
-//        parseJson2(jsonObject,null);
-//    }
+	public void parseJson(JSONObject jsonObject) {
+		this.setObj(jsonObject);
+		parseJson(jsonObject, "");
+		getArrayJsonKey(appendKeys);
+	}
 
-    private void parseJson(JSONObject jsonObject, String appendKey) {
-        if (null == jsonObject)
-            return;
-        try {
-            Iterator<String> it = jsonObject.keys();
-            while (it.hasNext()) {
-                String key = it.next();
-                JSONArray array = jsonObject.optJSONArray(key);
-                appendKey = key;
-                if (null == array) {
-                    String value = jsonObject.optString(key);
-                    if (value != null && !value.contains("{")) {
-                        if (false == flg) {
-                            h_jsonMap.put(appendKey, value.toString());
-                        }
-                        jsonMap.put(appendKey, value);
-                        jsonKey.add(appendKey);
-                    } else {
-                        parseJson(getJSONObject(value), appendKey);
-                    }
-                } else {
-                    parseJsonArray(array, -1, layer);
-                }
-            }
-            flg = true;
-        } catch (Exception e) {
-            e.toString();
-        }
-    }
+	private void parseJson(JSONObject jsonObject, String appendKey) {
+		if (null == jsonObject)
+			return;
+		try {
+			Iterator<String> it = jsonObject.keys();
+			while (it.hasNext()) {
+				String key = it.next();
+				JSONArray array = jsonObject.optJSONArray(key);
+				if ("" != getArrayJsonKey(appendKeys)) {
+					appendKeys.add("/");
+					appendKeys.add(key);
+					appendKey = getArrayJsonKey(appendKeys);
+				} else {
+					appendKey = key;
+				}
+				if (null == array) {
+					String value = jsonObject.optString(key);
+					if (value != null && !value.contains("{")) {
+						System.out.println("jsonMap_appendKey--------:"
+								+ appendKey);
+						jsonMap.put(appendKey, value);
+					} else {
+						appendKeys.add("/");
+						appendKeys.add(key);
+						parseJson(getJSONObject(value), appendKey);
+					}
+				} else {
+					if ("" != getArrayJsonKey(appendKeys)) {
+						appendKeys.add("/");
+						appendKeys.add(key);
+						appendKey = getArrayJsonKey(appendKeys);
+					} else {
+						appendKeys.add(key);
+						appendKey = key;
+					}
+					System.out.println("arrayKey:"
+							+ getArrayJsonKey(appendKeys));
+					parseJsonArray(array);
+				}
+			}
+		} catch (Exception e) {
+			e.toString();
+		}
+	}
 
-    public JSONObject getJSONObject(String json) {
-        try {
-            JSONObject result = new JSONObject(json);
-            return result;
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+	public JSONObject getJSONObject(String json) {
+		try {
+			JSONObject result = new JSONObject(json);
+			return result;
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
-    private int parseJsonArray(JSONArray array, int index, int layer) {
-        if (null == array)
-            return layer;
-        int lastLayer = layer++;
-        int len = array.length();
-        for (int i = 0; i < len; i++) {
-            JSONObject obj = (JSONObject) array.opt(i);
-            @SuppressWarnings("unchecked")
-            Iterator<String> it = obj.keys();
-            while (it.hasNext()) {
-                String key = it.next();
-                JSONArray a = obj.optJSONArray(key);
-                if (a != null)
-                    parseJsonArray(a, i, layer);
-                else {
-                    JSONObject jsonObject = getJSONObject(obj.optString(key));
-                    String jsonKey = i + "/" + lastLayer + "/" + key;
-                    parseJson(jsonObject, jsonKey);
-                }
-            }
-        }
-        return layer;
-    }
+	private int parseJsonArray(JSONArray array) {
+		if (null == array)
+			return -1;
+		int len = array.length();
+		for (int i = 0; i < len; i++) {
+			JSONObject obj = (JSONObject) array.opt(i);
+			@SuppressWarnings("unchecked")
+			Iterator<String> it = obj.keys();
+			while (it.hasNext()) {
+				String key = it.next();
+				JSONArray a = obj.optJSONArray(key);
+				if (a != null) {
+					appendKeys.add("/");
+					appendKeys.add(i + "");
+					appendKeys.add("/");
+					appendKeys.add(key);
+					parseJsonArray(a);
+				} else {
+					appendKeys.add("/");
+					appendKeys.add(i + "");
+					appendKeys.add("/");
+					appendKeys.add(key);
+					String jsonKey2 = getArrayJsonKey(appendKeys);
+					String value = obj.optString(key);
+					if (value != null && !value.contains("{")) {
+						System.out.println("jsonMap_jsonKey2--------:"
+								+ jsonKey2);
+						jsonMap.put(jsonKey2, value);
+						appendKeys.remove(appendKeys.size() - 1);
+						appendKeys.remove(appendKeys.size() - 1);
+						appendKeys.remove(appendKeys.size() - 1);
+						appendKeys.remove(appendKeys.size() - 1);
+					} else {
+						parseJson(getJSONObject(value), jsonKey2);
+					}
+				}
+			}
+		}
+		appendKeys.remove(appendKeys.size() - 1);
+		return 1;
+	}
 
-    public ArrayList<String> getJsonKey() {
-        return this.jsonKey;
-    }
+	public String getArrayJsonKey(ArrayList<String> appendKeys) {
+		String arrayJsonKey = "";
+		for (int i = 0; i < appendKeys.size(); i++) {
+			// System.out.println(appendKeys.get(i));
+			arrayJsonKey = arrayJsonKey + appendKeys.get(i);
+		}
+		System.out.println("appendKeys--------:" + arrayJsonKey);
+		return arrayJsonKey;
+	}
 
-    public HashMap<String, String> getJSONMap() {
-        return jsonMap;
-    }
+	public HashMap<String, String> getJSONMap() {
+		return jsonMap;
+	}
 
-    public HashMap<String, String> getH_jsonMap() {
-        return h_jsonMap;
-    }
-
-    public void setH_jsonMap(HashMap<String, String> h_jsonMap) {
-        this.h_jsonMap = h_jsonMap;
-    }
 }
