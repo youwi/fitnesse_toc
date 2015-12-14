4a99113e2c294aab241ab64faca8ccf1fd7d84e4
youwi <youwi@github.com>
2015-12-14 15:06:52 +0800

    注释之前的修改

--- src/main/java/TestHttpClient/HttpClient.java
+++ src/main/java/TestHttpClient/HttpClient.java
@@ -75,8 +75,8 @@ public class HttpClient {
 			setResponseBody(httpclient.execute(httpPost, responseHandler));
 			System.out.println("-------------------------------------------");
 			// System.out.println(getResponseBody());
-			String responseBody2 = new String (responseBody.getBytes("ISO-8859-1"),"utf-8");
-			System.out.println("responseBody: " + responseBody2);
+			//String responseBody2 = new String (responseBody.getBytes("ISO-8859-1"),"utf-8");
+			System.out.println("responseBody: " + responseBody);
 			System.out.println("-------------------------------------------");
 			return responseBody;
 		} finally {
