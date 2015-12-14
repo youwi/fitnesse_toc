0ba3127e8a34cdb3af48c36939156ac879f79da0
youwi <youwi@github.com>
2015-12-14 15:29:50 +0800

    修复中文乱码

--- src/main/java/TestHttpClient/HttpClient.java
+++ src/main/java/TestHttpClient/HttpClient.java
@@ -74,10 +74,8 @@ public class HttpClient {
 
 			setResponseBody(httpclient.execute(httpPost, responseHandler));
 			System.out.println("-------------------------------------------");
-			// System.out.println(getResponseBody());
-			String responseBody2 = new String (responseBody.getBytes("ISO-8859-1"),"utf-8");
-			//
-			System.out.println("responseBody: " + responseBody2);
+			// System.out.println(getResponseBody());	
+			System.out.println("responseBody: " + new String (responseBody.getBytes("ISO-8859-1"),"utf-8"));
 			System.out.println("-------------------------------------------");
 			return responseBody;
 		} finally {
