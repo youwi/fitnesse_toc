b4bce80c194e1b1ce57b4c24f33c6a00b4cceab5
youwi <youwi@github.com>
2016-11-23 11:18:15 +0800

    变更请求方式

--- src/main/java/com/qa/MonkeyKingBackstageServer/MonkeyKingBackstageServer.java
+++ src/main/java/com/qa/MonkeyKingBackstageServer/MonkeyKingBackstageServer.java
@@ -5,6 +5,7 @@ import com.qa.TestHttpClient.HttpRequestCallback;
 import com.qa.constants.ConfigConstants;
 import com.qa.utils.Data;
 import com.qa.utils.JSONParse;
+
 import org.json.JSONObject;
 
 import java.io.IOException;
@@ -17,18 +18,33 @@ public class MonkeyKingBackstageServer {
 	String URL;
 	String env = null;
 	String responseBody = null;
-	HttpClient testRequst = new HttpClient();
+	String type = null;
+	static HttpClient testRequst ;
+	
 
 	public MonkeyKingBackstageServer(String URL) {
+		if(testRequst==null)
+			testRequst=new HttpClient();
 		this.data = new Data();
 		this.jp = new JSONParse();
 		this.URL = URL;
 	}
-	public MonkeyKingBackstageServer(String URL, String env) {
+	public MonkeyKingBackstageServer(String URL,String env) {
+		if(testRequst==null)
+			testRequst=new HttpClient();
 		this.data = new Data();
 		this.jp = new JSONParse();
 		this.URL = URL;
 		this.env = env;
+	}
+	public MonkeyKingBackstageServer(String URL,String env,String type) {
+		if(testRequst==null)
+			testRequst=new HttpClient();
+		this.data = new Data();
+		this.jp = new JSONParse();
+		this.URL = URL;
+		this.env = env;
+		this.type = type;
 	}
 
 	public void setJsonParam(String json)
@@ -59,7 +75,7 @@ public class MonkeyKingBackstageServer {
 			
 			
 			if(null == env||"test".equals(env.toLowerCase())){
-			 responseBody = testRequst.httpPostRequest(
+			 responseBody = testRequst.httpRequest(
 					ConfigConstants.Monkey_King_Backstage_Server_BASE_URL + URL,
 					new HttpRequestCallback() {
 						@Override
@@ -79,11 +95,11 @@ public class MonkeyKingBackstageServer {
 							// TODO Auto-generated method stub
 							return data.getJsonParam();
 						}
-					});
+					},type);
 			}else if("sim".equals(env.toLowerCase()))
 			{
-				 responseBody = testRequst.httpPostRequest(
-    					ConfigConstants.MK_APP_SERVER_SIM_BASE_URL + URL,
+				 responseBody = testRequst.httpRequest(
+    					ConfigConstants.Monkey_King_Backstage_Server_BASE_URL + URL,
     					new HttpRequestCallback() {
     						@Override
     						public String addParam() {
@@ -103,7 +119,7 @@ public class MonkeyKingBackstageServer {
 								// TODO Auto-generated method stub
 								return data.getJsonParam();
 							}
-    					});
+    					},type);
 			}else{
 				System.out.println("env name error");
 	        	return false;
