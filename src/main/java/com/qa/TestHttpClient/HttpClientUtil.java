eb7e3159363bb4b6fbc24d72dd21dc7b7b164c27
youwi <youwi@github.com>
2016-11-29 12:25:08 +0800

    fix ssl bugs

--- src/main/java/com/qa/TestHttpClient/HttpClientUtil.java
+++ src/main/java/com/qa/TestHttpClient/HttpClientUtil.java
@@ -2,6 +2,8 @@ package com.qa.TestHttpClient;
 
 import java.io.File;
 import java.io.IOException;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -19,12 +21,16 @@ import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.entity.mime.MultipartEntityBuilder;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.apache.http.ssl.SSLContexts;
+import org.apache.http.ssl.TrustStrategy;
 import org.apache.http.util.EntityUtils;
 import org.apache.poi.hssf.extractor.ExcelExtractor;
 
@@ -34,206 +40,208 @@ import javax.net.ssl.X509TrustManager;
 
 public class HttpClientUtil {
 
-	private CloseableHttpClient httpclient = null;
+    private CloseableHttpClient httpclient = null;
 
-	private String responseBody = null;
-	private long responseTime = 999999999;
-	int timeout = 5;
+    private String responseBody = null;
+    private long responseTime = 999999999;
+    int timeout = 5;
 
-	@SuppressWarnings("deprecation")
-	public HttpClientUtil() {
-		SSLContext ctx = null;
-		try {
-			ctx = SSLContext.getInstance("TLS");
+    @SuppressWarnings("deprecation")
+    public HttpClientUtil() {
+        SSLContext ctx = null;  // 4.3
+        SSLContext sslContext=null;// 4.4
+        try {
+            ctx = SSLContext.getInstance("SSL");
+            X509TrustManager tm = new X509TrustManager() {
+                public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+                public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                }
+                public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                    return null;
+                }
+                public boolean isServerTrusted(java.security.cert.X509Certificate[] certs) {
+                    return true;
+                }
+                public boolean isClientTrusted(java.security.cert.X509Certificate[] certs) {
+                    return true;
+                }
+            };
+            ctx.init(null, new TrustManager[]{tm}, null);
 
-		X509TrustManager tm = new X509TrustManager()
-		{
-			public void checkClientTrusted(X509Certificate[] chain,  String authType) throws CertificateException
-			{
+            sslContext = SSLContexts.custom()
+                    .loadTrustMaterial(null, new TrustStrategy() {
+                        @Override
+                        public boolean isTrusted(final X509Certificate[] chain, final String authType) throws CertificateException {
+                            return true;
+                        }
+                    })
+                    //.useTLS()
+                    .build();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-			}
-			public void checkServerTrusted(X509Certificate[] chain,  String authType) throws CertificateException
-			{
+       // HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+        RequestConfig config = RequestConfig.custom()
+                .setConnectTimeout(timeout * 1000)
+                .setConnectionRequestTimeout(timeout * 1000)
+                .setSocketTimeout(timeout * 1000).build();
+        CloseableHttpClient client ;//= HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE);
+        PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
+        client = HttpClients.custom()
+                //	.setKeepAliveStrategy(myStrategy)
+           //     .setSslcontext(sslContext)
+                .setSSLSocketFactory(sslsf)
+                .setDefaultRequestConfig(config)
+        //        .setConnectionManager(connManager)  // bug? not trust
+                .setDefaultRequestConfig(config)
+                .setConnectionManagerShared(true)
+                .build();
 
-			}
-			public X509Certificate[] getAcceptedIssuers()
-			{
-				return null;
-			}
-		};
-		ctx.init(null, new TrustManager[]{tm}, null);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+        this.httpclient = client;
+        //httpClientBuilder.setSSLContext(ctx);
+        //httpClientBuilder.setSslcontext(ctx);//not work on 4.4
+        //this.httpclient = httpClientBuilder.build();
+        //this.httpclient = HttpClients.createDefault();
+    }
 
-		HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
-		RequestConfig config = RequestConfig.custom()
-				.setConnectTimeout(timeout * 1000)
-				.setConnectionRequestTimeout(timeout * 1000)
-				.setSocketTimeout(timeout * 1000).build();
-		CloseableHttpClient client =HttpClientBuilder.create().setDefaultRequestConfig(config).build();
+    public String httpPostRequest(String URL, HttpRequestCallback ci) throws IOException {
+        try {
 
-		PoolingHttpClientConnectionManager connManager = new PoolingHttpClientConnectionManager();
-		//connManager.set
-		client = HttpClients.custom()
-			//	.setKeepAliveStrategy(myStrategy)
-				.setConnectionManager(connManager)
-				.setDefaultRequestConfig(config)
-				.setConnectionManagerShared(true)
-				.build();
-
-		this.httpclient=client;
-		//httpClientBuilder.setSSLContext(ctx);
-		httpClientBuilder.setSslcontext(ctx);
-		// this.httpclient = httpClientBuilder.build();
-		//this.httpclient = HttpClients.createDefault();
-	}
-
-	public String httpPostRequest(String URL, HttpRequestCallback ci) throws IOException {
-		try {
-			
-			Iterator<Map.Entry<String, String>> iter = ci.AddHeaderParameters();
-			HttpPost httpPost = new HttpPost(URL);
-			httpPost.addHeader("Content-Type", "application/json;charset=UTF-8");
+            Iterator<Map.Entry<String, String>> iter = ci.AddHeaderParameters();
+            HttpPost httpPost = new HttpPost(URL);
+            httpPost.addHeader("Content-Type", "application/json;charset=UTF-8");
 //			httpPost.addHeader("User-ID", "0");
-			int flag = 0;
-			int CTFlag = 0;
-			while (iter.hasNext()) {
-				Map.Entry<String, String> me = iter.next();
-				httpPost.addHeader(me.getKey(), me.getValue());
-				System.out.println("请求头 Key： "+me.getKey()+"------请求头 Value： "+me.getValue());
-				if("os".equals(me.getKey()))
-				{
-					flag = 1;
-				}
-				if("Content-Type".equals(me.getKey()))
-				{
-					CTFlag = 1;
-				}
-			}
-			if(0 == flag)
-			{
-				httpPost.addHeader("os", "monitor");
+            int flag = 0;
+            int CTFlag = 0;
+            while (iter.hasNext()) {
+                Map.Entry<String, String> me = iter.next();
+                httpPost.addHeader(me.getKey(), me.getValue());
+                System.out.println("请求头 Key： " + me.getKey() + "------请求头 Value： " + me.getValue());
+                if ("os".equals(me.getKey())) {
+                    flag = 1;
+                }
+                if ("Content-Type".equals(me.getKey())) {
+                    CTFlag = 1;
+                }
+            }
+            if (0 == flag) {
+                httpPost.addHeader("os", "monitor");
 //				System.out.println("请求头 Key： "+"os"+"------请求头 Value： "+"monitor");
-			}
-			if(0 == CTFlag)
-			{
-				httpPost.addHeader("Content-Type", "application/json;charset=UTF-8");
-			}
+            }
+            if (0 == CTFlag) {
+                httpPost.addHeader("Content-Type", "application/json;charset=UTF-8");
+            }
 
-			if(null != ci.addParam())
-			{
-			System.out.println("请求参数：  "+ci.addParam());
-			httpPost.setEntity(new StringEntity(ci.addParam()));
-			}
-			
-			if(null != ci.addJsonParam())
-			{
-				System.out.println("请求参数：  "+ci.addJsonParam());
-				httpPost.setEntity(new StringEntity(ci.addJsonParam()));
-			}
-			
-			long temp = System.currentTimeMillis();
-			// Before end
-			System.out.println("请求地址：  " + httpPost.getURI());
-			ResponseHandler<String> responseHandler =createResponseHandler();
+            if (null != ci.addParam()) {
+                System.out.println("请求参数：  " + ci.addParam());
+                httpPost.setEntity(new StringEntity(ci.addParam()));
+            }
 
-			setResponseBody(httpclient.execute(httpPost, responseHandler));
-			System.out.println("-------------------------------------------");
+            if (null != ci.addJsonParam()) {
+                System.out.println("请求参数：  " + ci.addJsonParam());
+                httpPost.setEntity(new StringEntity(ci.addJsonParam()));
+            }
 
-			responseTime = System.currentTimeMillis() - temp;
-			// System.out.println(getResponseBody());	
-			//System.out.println("responseBody: " + new String (responseBody.getBytes("ISO-8859-1"),"utf-8"));
-			System.out.println("完整响应体： " + responseBody);
+            long temp = System.currentTimeMillis();
+            // Before end
+            System.out.println("请求地址：  " + httpPost.getURI());
+            ResponseHandler<String> responseHandler = createResponseHandler();
+
+            setResponseBody(httpclient.execute(httpPost, responseHandler));
+            System.out.println("-------------------------------------------");
+
+            responseTime = System.currentTimeMillis() - temp;
+            // System.out.println(getResponseBody());
+            //System.out.println("responseBody: " + new String (responseBody.getBytes("ISO-8859-1"),"utf-8"));
+            System.out.println("完整响应体： " + responseBody);
 //			System.out.println("responseBody: " + new String (responseBody.getBytes("ISO-8859-1"),"utf-8"));
-			System.out.println("-------------------------------------------");
-			return responseBody;
-		} finally {
-			httpclient.close();
-		}
-	}
+            System.out.println("-------------------------------------------");
+            return responseBody;
+        } finally {
+            httpclient.close();
+        }
+    }
 
-	public String httpGetRequest(String URL, HttpRequestCallback ci)
-			throws IOException {
-		try {
+    public String httpGetRequest(String URL, HttpRequestCallback ci)
+            throws IOException {
+        try {
 
-			Iterator<Map.Entry<String, String>> iter = ci.AddHeaderParameters();
-			HttpGet httpGet = new HttpGet(URL);
+            Iterator<Map.Entry<String, String>> iter = ci.AddHeaderParameters();
+            HttpGet httpGet = new HttpGet(URL);
 
-			httpGet.addHeader("Content-Type", "application/json;charset=UTF-8");
+            httpGet.addHeader("Content-Type", "application/json;charset=UTF-8");
 //			httpPost.addHeader("User-ID", "0");
-			int flag = 0;
-			while (iter.hasNext()) {
-				Map.Entry<String, String> me = iter.next();
-				httpGet.addHeader(me.getKey(), me.getValue());
-				System.out.println("请求头 Key： "+me.getKey()+"------请求头 Value： "+me.getValue());
-				if("os".equals(me.getKey()))
-				{
-					flag = 1;
-				}
-			}
-			if(0 == flag)
-			{
-				httpGet.addHeader("os", "monitor");
+            int flag = 0;
+            while (iter.hasNext()) {
+                Map.Entry<String, String> me = iter.next();
+                httpGet.addHeader(me.getKey(), me.getValue());
+                System.out.println("请求头 Key： " + me.getKey() + "------请求头 Value： " + me.getValue());
+                if ("os".equals(me.getKey())) {
+                    flag = 1;
+                }
+            }
+            if (0 == flag) {
+                httpGet.addHeader("os", "monitor");
 //				System.out.println("请求头 Key： "+"os"+"------请求头 Value： "+"monitor");
-			}
+            }
 
 //			if(null != ci.addParam())
 //			{
 //			System.out.println("请求参数：  "+ci.addParam());
 //
 //			}
-			long temp = System.currentTimeMillis();
-			ResponseHandler<String> responseHandler =createResponseHandler();
-			setResponseBody(httpclient.execute(httpGet, responseHandler));
-			System.out.println("-------------------------------------------");
-			System.out.println(getResponseBody());
-			System.out.println("-------------------------------------------");
+            long temp = System.currentTimeMillis();
+            ResponseHandler<String> responseHandler = createResponseHandler();
+            setResponseBody(httpclient.execute(httpGet, responseHandler));
+            System.out.println("-------------------------------------------");
+            System.out.println(getResponseBody());
+            System.out.println("-------------------------------------------");
 
-			return responseBody;
-		} finally {
-			httpclient.close();
-		}
-	}
-	ResponseHandler createResponseHandler(){
-		return  new ResponseHandler<String>() {
-			public String handleResponse(final HttpResponse response)
-					throws ClientProtocolException, IOException {
-				int status = response.getStatusLine().getStatusCode();
-				if (status >= 200 && status < 300) {
-					HttpEntity entity = response.getEntity();
-					return entity != null ? EntityUtils.toString(entity)
-							: null;
-				} else {
-					throw new ClientProtocolException(
-							"Unexpected response status: " + status);
-				}
-			}
-		};
-	}
+            return responseBody;
+        } finally {
+            httpclient.close();
+        }
+    }
 
-	public String httpRequest(String URL, HttpRequestCallback ci, String type) throws IOException
-	{
-		if("get".equals(type.toLowerCase()))
-		{
-			return httpGetRequest(URL,ci);
-		}else if("post".equals(type.toLowerCase()))
-		{
-			return httpPostRequest(URL,ci);
-		}else{
-			return null;
-		}
-	}
+    ResponseHandler createResponseHandler() {
+        return new ResponseHandler<String>() {
+            public String handleResponse(final HttpResponse response)
+                    throws ClientProtocolException, IOException {
+                int status = response.getStatusLine().getStatusCode();
+                if (status >= 200 && status < 300) {
+                    HttpEntity entity = response.getEntity();
+                    return entity != null ? EntityUtils.toString(entity)
+                            : null;
+                } else {
+                    throw new ClientProtocolException(
+                            "Unexpected response status: " + status);
+                }
+            }
+        };
+    }
 
-	public String getResponseBody() {
-		return responseBody;
-	}
+    public String httpRequest(String URL, HttpRequestCallback ci, String type) throws IOException {
+        if ("get".equals(type.toLowerCase())) {
+            return httpGetRequest(URL, ci);
+        } else if ("post".equals(type.toLowerCase())) {
+            return httpPostRequest(URL, ci);
+        } else {
+            return null;
+        }
+    }
 
-	public void setResponseBody(String responseBody) {
-		this.responseBody = responseBody;
-	}
+    public String getResponseBody() {
+        return responseBody;
+    }
 
-	public long getResponseTime() {
-		return responseTime;
-	}
+    public void setResponseBody(String responseBody) {
+        this.responseBody = responseBody;
+    }
+
+    public long getResponseTime() {
+        return responseTime;
+    }
 }
---
+++ src/test/java/com/qa/BaseServerTest2.java
@@ -0,0 +1,49 @@
+package com.qa;
+
+import com.qa.HouseholderAppServer.HouseholderAppServer;
+import com.qa.LegalMgmtSys.LegalMgmtSys;
+import com.qa.exampleAppServer.ExampleAppServer;
+import org.junit.Test;
+
+/**
+ * Created by yus on 2016/11/26.
+ */
+public class BaseServerTest2 {
+
+    @Test
+    public void testFun() throws Exception {
+        HouseholderAppServer s2=new HouseholderAppServer("/dicArea/getCityList.rest");
+        s2.setParam("searchStr","天山","String");
+        s2.testRun("message,status,data");
+        s2.getParam("status" );
+
+
+        LegalMgmtSys s=new LegalMgmtSys("/dicArea/getCityList.rest");
+        s.setHeaderParam("HOST","yun2.test.wkzf");
+        s.setHeaderParam("Cookie","wksso=c713a8e3-cddd-4c4e-be8e-ae8c2b330144");
+        s.setParam("cityId","43","int");
+        s.testRun("message,status,data");
+        s.getParam("status" );
+
+
+
+      /*          householder app server	/dicArea/getCityList.rest
+        ensure	testRun;
+        check	getParam	status	[null] expected [1]*/
+
+     /*   setParam;	cityid	43	int
+        setParam;	searchStr	天山	String
+        ensure	testRun;	message,status,data/*/
+    }
+
+/*    script	legalMgmtSys	/legalmgmt2/contractManagement/signJusticeGroup/getGroupList.action	test	post
+            setHeaderParam;	Host	yun2.test.wkzf
+            setHeaderParam;	Cookie	wksso=c713a8e3-cddd-4c4e-be8e-ae8c2b330144
+            setParam;	cityId	43	int
+    ensure	testRun;	status,message,data
+    check	getParam	status	1
+    check	getParam	message*/
+
+
+
+}
