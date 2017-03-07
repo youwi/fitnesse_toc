4023803702f42eb4e7ca14aa34978d260ad877a1
youwi <youwi@github.com>
2017-03-07 11:33:26 +0800

    修正ip地址

--- src/main/java/com/qa/constants/ConfigConstantsTest.java
+++ src/main/java/com/qa/constants/ConfigConstantsTest.java
@@ -53,11 +53,11 @@ public class ConfigConstantsTest {
 
     public final static String NEW_COMMUNITY_APP_SERVER_TEST_BASE_URL = "https://";
 
-    public final static String HOUSE_SYS_SERVER_TEST_BASE_URL = "https://";
+    public final static String HOUSE_SYS_SERVER_TEST_BASE_URL = "https://10.0.18.79:8192";
 
     public final static String RATE_SYS_SERVER_TEST_BASE_URL = "https://";
 
-    public final static String OPMGMT2_SYS_SERVER_TEST_BASE_URL = "https://";
+    public final static String OPMGMT2_SYS_SERVER_TEST_BASE_URL = "https:/10.0.18.79:8193/";
 
     public final static String JDBC_DRIVER_CLASS = "com.mysql.jdbc.Driver";
     public final static String JDBC_CONNECT_URL = "jdbc:mysql://10.0.18.188:3306/milestone?useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false";
