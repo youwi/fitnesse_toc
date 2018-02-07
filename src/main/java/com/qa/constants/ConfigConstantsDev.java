9ac9c5f86bc5dca72e7ec070b70264bf8a59b756
chenll <chenll@lieluobo.com>
2018-02-07 21:44:13 +0800

    reset ConfigConstantsDev

--- src/main/java/com/qa/constants/ConfigConstantsDev.java
+++ src/main/java/com/qa/constants/ConfigConstantsDev.java
@@ -2,8 +2,8 @@ package com.qa.constants;
 
 public class ConfigConstantsDev {
 
-    public final static String LIE_LUO_BO_CW_URL="http://cw.lieluobo.developing";
-    public final static String LIE_LUO_BO_HR_URL="http://lieluobo.developing";
+    public final static String LIE_LUO_BO_CW_URL="http://cw.dev.haolie.cn";
+    public final static String LIE_LUO_BO_HR_URL="http://hr.dev.haolie.cn";
     public final static String LIE_LUO_BO_WWW_URL="http://am2-dev.imd.com";
     public final static String AUTOMIND_WWW_URL="http://am2-test.imd.com";
 
@@ -11,7 +11,7 @@ public class ConfigConstantsDev {
 
 
     public final static String JDBC_DRIVER_CLASS = "com.mysql.jdbc.Driver";
-    public final static String JDBC_CONNECT_URL = "jdbc:mysql://172.16.52.81:3306/dev?useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false";
-    public final static String JDBC_USERNAME = "dev";
-    public final static String JDBC_PASSWORD = "haolie123";
- }
+    public final static String JDBC_CONNECT_URL = "jdbc:mysql://10.0.18.188:3306/milestone?useUnicode=true&characterEncoding=utf-8&autoReconnect=true&failOverReadOnly=false";
+    public final static String JDBC_USERNAME = "guwen";
+    public final static String JDBC_PASSWORD = "guwen@lifang.com";
+}
